59b1b5d4878b967c8552c7dfc9cff95354e21674
zztkm <zztkm@tsurutatakumi.info>
2025-05-17 10:45:50 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Sora Android SDK が JitPack で提供されているため追加
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "SoraChatLabo"
+include(":app")
+ 
